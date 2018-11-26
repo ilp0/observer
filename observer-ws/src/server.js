9e8b4bcb05d04d184d59318cbf895205591fd173
Oskari Seppä <oskari.seppa@leapsmith.com>
2018-11-26 15:25:48 +0200

    Backend for editing friendlyname

--- observer-ws/src/server.js
+++ observer-ws/src/server.js
@@ -328,6 +328,13 @@ function parse_message (cli, message) {
                 });
                 
             }
+            else if (jsn['cmd'] == "MISC") {
+                if(jsn['sub'] == "SETN") {
+                    let pkey = jsn['pkey'];
+                    let friendlyname = jsn['friendlyname'];
+                    con.query("INSERT INTO slave (friendlyname)" + "VALUES (" + con.escape(friendlyname) + ")" + "WHERE uni_id=" + pkey);
+                    console.log(pkey + " name changed to " + friendlyname);
+                }
         }
     }
 
